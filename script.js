cebc610051ffcf8fb08085fdab9e64eb4e591d7c
shahadat10033 <shahadat10033@gmail.com>
2023-03-04 00:29:05 +0600

    modal displayed

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const displayData=(datas)=>{
     const cardContainer=document.getElementById("all-card-container")
     cardContainer.innerHTML="";
     datas.forEach(data => {
-        console.log(data)
         const div=document.createElement("div");
         div.classList.add("col-md-4");
         div.innerHTML=`
@@ -33,21 +32,42 @@ const displayData=(datas)=>{
           <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
          </svg> <span> ${data.published_in}</span></p>
           </section>
-          <section class="rounded-5 btn btn-danger">
+          <section class="rounded-5 btn btn-danger "  id="openModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
           <svg class=" text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
             </svg>
       
           </section>
+
+
+          
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content" style="position:absolute;">
+        <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close" style="background:red;color:white;border-radius:50%; padding:0 8px 13px ; position:absolute; top:-11px; right:-11px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg></button>
+      <div class="modal-body">
+        ...
+      </div>
+      
+    </div>
+  </div>
+</div>
           
       </div>
         
         </div>
         `
-       
         cardContainer.appendChild(div);
-
-     
+       
+        
+        
+        
+        
+        
+        
       
       
 

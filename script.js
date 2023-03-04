ad26e9c3a53ab8b71179f0d2553128fc96cd368f
shahadat10033 <shahadat10033@gmail.com>
2023-03-04 07:37:00 +0600

    modal left card complete

--- script.js
+++ script.js
@@ -1,13 +1,77 @@
 const loadData =()=>{
     fetch("https://openapi.programming-hero.com/api/ai/tools")
      .then(response => response.json())
-    .then(data => displayData(data.data.tools.slice(0,6)));
+    .then(data => displayData(data.data.tools.slice(0,6) ));
 }
-const displayData=(datas)=>{
+
+
+const openModal =(id)=>{ 
+  if(id<10){ 
+   fetch(`https://openapi.programming-hero.com/api/ai/tool/0${id}`)
+.then(response => response.json())
+.then(singleData => displaySingleData(singleData.data))}
+else{ 
+   fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
+.then(response => response.json())
+.then(singleData => displaySingleData(singleData.data))}
+
+}
+displaySingleData=(singleData)=>{
+  console.log(singleData)
+  const description=document.getElementById("description");
+  description.innerHTML=`${singleData.description}`
+  
+    
+
+const cardText=document.getElementById("card-text");
+cardText.innerHTML=`
+<div class="text-primary " style="width:45px">${singleData.pricing[0].price}<br>${singleData.pricing[0].plan}</div>
+<div class="text-danger "style="width:45px">${singleData.pricing[1].price}<br>${singleData.pricing[1].plan}</div>
+<div class="text-dark "style="width:45px">${singleData.pricing[2].price}<br>${singleData.pricing[2].plan}</div>`
+ 
+const featureUl=document.getElementById("featureUl")
+featureUl.innerHTML=`
+<li>${singleData.features[1].feature_name}</li>
+<li>${singleData.features[2].feature_name}</li>
+<li>${singleData.features[3].feature_name}</li>
+`
+
+const integrations=document.getElementById("integrations")
+integrations.innerHTML=`
+<li>${singleData.integrations[0]}</li>
+<li>${singleData.integrations[1]}</li>
+<li>${singleData.integrations[2]}</li>
+
+`
+}  
+
+
+
+const displayData=(datas) =>{
     const cardContainer=document.getElementById("all-card-container")
     cardContainer.innerHTML="";
     datas.forEach(data => {
+    //   console.log(data.features)
+
+    //   const feature=document.getElementById("feature");
+    //   data.features.forEach(featurelist => {
+       
+    //       console.log(featurelist)
+          
+  
+    // let li =document.createElement("li")
+    // li.innerText=`${featurelist}`
+    // feature.appendChild(li);
+    
+    
+          
+    //   })
+      
+     
+
+      
         const div=document.createElement("div");
+        
         div.classList.add("col-md-4");
         div.innerHTML=`
         <div class="border g-2 my-3 shadow-lg p-3 mb-5"style="height:550px ">
@@ -16,12 +80,12 @@ const displayData=(datas)=>{
         <div class=" m-3"><span class="fw-semibold">Features</span> 
         <br>
        
-        <ol >
-        <li>${data.features[0]}</li>
-        <li>${data.features[1]}</li>
-        <li>${data.features[2]}</li>
+        <ol id="feature">
+        <li>"${data.features[0]}"</li>
+         <li>"${data.features[1]}"</li>
+         <li>"${data.features[2]}"</li>
         </ol>
-        
+         
         </div>
         <hr>
         <div class="d-flex justify-content-between align-items-center container">
@@ -32,24 +96,56 @@ const displayData=(datas)=>{
           <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
          </svg> <span> ${data.published_in}</span></p>
           </section>
-          <section class="rounded-5 btn btn-danger "  id="openModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <section onclick="openModal(${parseFloat(data.id)})" class="rounded-5 btn btn-danger "   data-bs-toggle="modal" data-bs-target="#exampleModal">
           <svg class=" text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
             </svg>
       
           </section>
 
+          <!-- modal-->
 
-          
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content" style="position:absolute;">
         <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close" style="background:red;color:white;border-radius:50%; padding:0 8px 13px ; position:absolute; top:-11px; right:-11px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
       </svg></button>
       <div class="modal-body">
-        ...
+        
+      <div class="row">
+      <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"id="description"></h5>
+            <div class="card-text d-flex justify-content-between container  my-5"  id="card-text">
+                
+            </div>
+            <div class="d-flex justify-content-between">
+    <div>
+      <h5 >Feature</h5>
+      <ul id="featureUl"></ul>
+    </div>
+    <div>
+      <h5>Integrations</h5>
+      <ul id="integrations"></ul>
+    </div>
+    
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
       </div>
       
     </div>
@@ -59,10 +155,14 @@ const displayData=(datas)=>{
       </div>
         
         </div>
+
+
         `
-        cardContainer.appendChild(div);
-       
         
+        cardContainer.appendChild(div);
+        
+        
+      
         
         
         
@@ -76,6 +176,10 @@ const displayData=(datas)=>{
 
 }
 
+
+
+
+
 const loadAllData=()=>{
 
   fetch("https://openapi.programming-hero.com/api/ai/tools")
@@ -88,3 +192,7 @@ const loadAllData=()=>{
 }
 
 loadData()
+
+
+
+ 
